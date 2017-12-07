43dd3fc542670f1817e04ba8b1ee3592c129a4df
Eric Korolev <3254@mail.ru>
2017-12-07 10:23:45 +0500

    Echo string with dynamic numbers

--- helper.php
+++ helper.php
@@ -3,23 +3,147 @@
  * Вспомогательный класс для модуля "История школы в статистике".
  */
  
-class ModRatingHelper
+class ModHistoryHelper
 { 
-    public static function getRating()
+    /**
+      * Метод считает количество учеников.
+      * Ученик - это зарегистрированный пользователь, который оплатил обучение.
+      * Оплата обучения - это в столбце количество пожертвований(donat) больше нуля.
+      */
+    public static function countStudents()
     {
         $db = JFactory::getDbo();
 
 		$query = $db->getQuery(true);
 
-		$query->select('username,(num_contacts + (num_dates * 10 ) + (num_closenesses * 100)) AS Practice');
+		$query->select('COUNT(email)');
 		$query->from($db->quoteName('#__users'));
-		$query->where($db->quoteName('num_contacts') . ' > '. $db->quote('0'));
-		$query->order('Practice DESC');
+		$query->where($db->quoteName('donat') . ' > ' . $db->quote('0'));
 
 		$db->setQuery($query);
 
-		$result = $db->loadRowList();
+		$result = $db->loadResult();
 
 		return $result;
+    }
+
+    /**
+     * Метод считает количество уникальных стран учеников.
+     * Есть ученики с незаполненной графой "страна" из-за чего значение = " ".
+     * Фильтруется значение " ", чтобы не считать его еще одной уникальной страной.
+     */
+    public static function countCountries()
+    {
+        $db = JFactory::getDbo();
+
+        $query = $db->getQuery(true);
+
+        $query->select('COUNT(DISTINCT country)');
+        $query->from($db->quoteName('#__users'));
+        $query->where($db->quoteName('country') . ' != ' . $db->quote(' '));
+
+        $db->setQuery($query);
+
+        $result = $db->loadResult();
+
+        return $result;
+    }
+
+    /**
+     * Метод считает количество уникальных городов учеников.
+     * Есть ученики с незаполненной графой "город" из-за чего значение = " ".
+     * Фильтруется значение " ", чтобы не считать его еще одной уникальным городом.
+     */
+    public static function countCities()
+    {
+        $db = JFactory::getDbo();
+
+        $query = $db->getQuery(true);
+
+        $query->select('COUNT(DISTINCT city)');
+        $query->from($db->quoteName('#__users'));
+        $query->where($db->quoteName('city') . ' != ' . $db->quote(' '));
+
+        $db->setQuery($query);
+
+        $result = $db->loadResult();
+
+        return $result;
+    }
+
+    /**
+     * Метод считает количество подходов, совершенных всеми учениками.
+     */
+    public static function countContacts()
+    {
+        $db = JFactory::getDbo();
+
+        $query = $db->getQuery(true);
+
+        $query->select('SUM(num_contacts)');
+        $query->from($db->quoteName('#__users'));
+
+        $db->setQuery($query);
+
+        $result = $db->loadResult();
+
+        return $result;
+    }
+
+    /**
+     * Метод считает количество свиданий, совершенных всеми учениками.
+     */
+    public static function countDates()
+    {
+        $db = JFactory::getDbo();
+
+        $query = $db->getQuery(true);
+
+        $query->select('SUM(num_dates)');
+        $query->from($db->quoteName('#__users'));
+
+        $db->setQuery($query);
+
+        $result = $db->loadResult();
+
+        return $result;
+    }
+
+    /**
+     * Метод считает количество секса, совершенных всеми учениками.
+     */
+    public static function countSex()
+    {
+        $db = JFactory::getDbo();
+
+        $query = $db->getQuery(true);
+
+        $query->select('SUM(num_closenesses)');
+        $query->from($db->quoteName('#__users'));
+
+        $db->setQuery($query);
+
+        $result = $db->loadResult();
+
+        return $result;
+    }
+
+    /**
+     * Метод считает сумму пожертвований, совершенных всеми учениками.
+     */
+    public static function countDonat()
+    {
+        $db = JFactory::getDbo();
+
+        $query = $db->getQuery(true);
+
+        $query->select('SUM(donat)');
+        $query->from($db->quoteName('#__users'));
+
+        $db->setQuery($query);
+
+        $result = $db->loadResult();
+
+        return $result;
     }
 }
--- mod_history.php
+++ mod_history.php
@@ -7,6 +7,18 @@ defined('_JEXEC') or die;
 
 require_once dirname(__FILE__) . '/helper.php';
 
-$rating = ModRatingHelper::getRating();
+$num_students = ModHistoryHelper::countStudents(); // количество учеников
+
+$num_countries = ModHistoryHelper::countCountries(); // количество стран
+
+$num_cities = ModHistoryHelper::countCities(); // количество городов
+
+$num_contacts = ModHistoryHelper::countContacts(); // количество подходов
+
+$num_dates = ModHistoryHelper::countDates(); // количество свиданий
+
+$num_sex = ModHistoryHelper::countSex(); // количество секса
+
+$num_donat = ModHistoryHelper::countDonat(); // сумма пожертвований
 
 require JModuleHelper::getLayoutPath('mod_history');
